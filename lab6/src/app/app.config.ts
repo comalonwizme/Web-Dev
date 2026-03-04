e453ed86c96a99e2de469e2bbd00c261f85d414c
comalonwizme <a_aliaidar@kbtu.kz>
2026-03-04 11:59:16 +0500

    upd

--- lab6/src/app/app.config.ts
+++ lab6/src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
